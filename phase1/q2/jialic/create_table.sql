a9e47b6594c034eff9e9486902cb0dd0c2cb0f3d
jaly50 <jialic@andrew.cmu.edu>
2015-03-07 23:38:38 -0500

    lala

---
+++ phase1/q2/jialic/create_table.sql
@@ -0,0 +1,21 @@
+USE mysqltwitter;
+DROP TABLE IF EXISTS `twitter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `twitter` 
+  (
+  `tweetid` varchar(19) NOT NULL,
+  `userid` text,
+  `time` text,
+  `text` text,
+  `score` int(11) DEFAULT NULL,
+  `censoredtext` text,
+  PRIMARY KEY (`tweetid`)
+)  CHARACTER SET utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+
+LOAD DATA LOCAL  INFILE '/Users/Scarlett/Desktop/eBusiness/15619/619Project/Phrase1/q2/db_1000_0.txt' INTO TABLE twitter FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\0\n';
+
+
