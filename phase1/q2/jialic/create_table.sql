5e1646ee750f067718063356fa9c4492c11d619d
jaly50 <jialic@andrew.cmu.edu>
2015-03-07 23:46:18 -0500

    database problem

--- phase1/q2/jialic/create_table.sql
+++ phase1/q2/jialic/create_table.sql
@@ -11,11 +11,11 @@ CREATE TABLE `twitter`
   `score` int(11) DEFAULT NULL,
   `censoredtext` text,
   PRIMARY KEY (`tweetid`)
-)  CHARACTER SET utf8;
+)  CHARACTER SET utf8 COLLATE utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
-
+ ALTER DATABASE `mysqltwitter` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 LOAD DATA LOCAL  INFILE '/Users/Scarlett/Desktop/eBusiness/15619/619Project/Phrase1/q2/db_1000_0.txt' INTO TABLE twitter FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\0\n';
 
 
