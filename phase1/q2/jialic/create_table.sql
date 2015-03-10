9d66109a7516990b2fca92119dbf7856568bc88d
EC2 Default User <ec2-user@ip-172-31-59-37.ec2.internal>
2015-03-10 05:33:32 +0000

    update

--- phase1/q2/jialic/create_table.sql
+++ phase1/q2/jialic/create_table.sql
@@ -16,10 +16,7 @@ CREATE TABLE `twitter`
 
 
  ALTER DATABASE `mysqltwitter` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-<<<<<<< HEAD
-LOAD DATA LOCAL  INFILE '/Users/Scarlett/Desktop/eBusiness/15619/619Project/Phrase1/q2/db_1000' INTO TABLE twitter FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\0\n';
-=======
-LOAD DATA LOCAL  INFILE '/home/ec2-user/rawdata' INTO TABLE twitter FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\0\n';
+LOAD DATA LOCAL  INFILE '/mnt/rawdata' INTO TABLE twitter FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\0\n';
 >>>>>>> 0b311e5f4f9ee39af08c3d5ba3789991a058ea21
 
 
