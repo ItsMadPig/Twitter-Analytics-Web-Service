1b03b62e691d3896855038e7bff13d77da013944
Jiali Chen <scarlett.chen50@gmail.com>
2015-03-12 23:05:11 -0400

    Update Test.java

--- phase1/q2/jialic/frontEnd/src/Test.java
+++ phase1/q2/jialic/frontEnd/src/Test.java
@@ -9,7 +9,8 @@ import io.undertow.util.Headers;
 public class Test {
 
 	public static void main(final String[] args) {
-		Undertow server = Undertow.builder().addHttpListener(8080, "0.0.0.0")
+		Undertow server = Undertow.builder().addHttpListener(80, "0.0.0.0") //For instance
+		//Undertow server = Undertow.builder().addHttpListener(8080, "0.0.0.0") //For local machine
 				.setHandler(new HttpHandler() {
 
 					public void handleRequest(final HttpServerExchange exchange)
@@ -26,13 +27,13 @@ public class Test {
 							        System.out.println("Proper Usage is: java Test <DNS name of mysql server>");
 							        System.exit(0);
 							    }
-							 long startTime = System.currentTimeMillis();
-							 //System.out.println(Arrays.toString(args));
+							 //long startTime = System.currentTimeMillis();
+						
 							 Q2 q2 = new Q2(args[0]);
 							 q2.processRequest(exchange);
-							 long endTime   = System.currentTimeMillis();
-							 long totalTime = endTime - startTime;
-							 System.out.println("runtime: "+totalTime);
+							// long endTime   = System.currentTimeMillis();
+							// long totalTime = endTime - startTime;
+							// System.out.println("runtime: "+totalTime);
 							 
 						 }
 						 else
