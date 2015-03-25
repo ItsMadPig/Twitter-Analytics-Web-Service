a3b2462e8077188dbb85ecf8321c5c7340637d92
jaly50 <jialic@andrew.cmu.edu>
2015-03-25 15:26:55 -0400

    q3.haha

---
+++ mysql/load_table/q3.sql
@@ -0,0 +1,15 @@
+USE mysqltwitter;
+DROP TABLE IF EXISTS `q3`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `q3` 
+  (
+  `x` BIGINT NOT NULL,
+  `y` TEXT NOT NULL
+)  CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+LOAD DATA LOCAL  INFILE '/Users/Scarlett/Documents/workspace/Q3ETL/src/q3data' INTO TABLE q3 FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n';
+
+
