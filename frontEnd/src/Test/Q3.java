c400a19b4114e7c5ddfacffc31d44cd92638d6ca
jaly50 <jialic@andrew.cmu.edu>
2015-03-31 07:45:02 -0700

    editing

--- frontEnd/src/Test/Q3.java
+++ frontEnd/src/Test/Q3.java
@@ -14,9 +14,7 @@ import java.util.Map;
 import java.lang.String;
 
 public class Q3 {
-	static private List<Connection> connectionPool = new ArrayList<Connection>();  
-	static private String jdbcDriver ="com.mysql.jdbc.Driver";
-	static private String jdbcURL = "jdbc:mysql://ec2-52-4-103-140.compute-1.amazonaws.com/mysqltwitter";
+	
 	
 	static private String tableName="q3";
 
