f1e0555070614f5c91d470c7b979869b9db5f57d
root <root@ip-172-31-57-239.ec2.internal>
2015-10-10 02:42:02 +0000

    TA

---
+++ TA/project/ETL/create_table.sql
@@ -0,0 +1,20 @@
+USE mysqltwitter;
+DROP TABLE IF EXISTS `twitter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `twitter_q2` 
+  (
+  `tweetid` varchar(19) NOT NULL,
+  `userid` varchar(19),
+  `time` text,
+  `text` text,
+  `score` int(11) DEFAULT NULL,
+  `censoredtext` text,
+  PRIMARY KEY (`tweetid`)
+)  CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+
+ ALTER DATABASE `mysqltwitter` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+LOAD DATA LOCAL  INFILE '/mnt/rawdata' INTO TABLE twitter FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\0\n';
+
