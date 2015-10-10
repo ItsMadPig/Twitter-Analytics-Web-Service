917a95f7f09cfd10b640f125bf6812b78944f49c
Ubuntu <ubuntu@ip-172-31-53-238.ec2.internal>
2015-10-10 08:50:58 +0000

    change use EBS

--- TA/mysqlUseEBS.sh
+++ TA/mysqlUseEBS.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 main(){
 sudo service mysql stop
-echo -e "/dev/xvdf\t/mnt\text4\tdefaults,nofail 0\t2" | sudo tee -a /etc/fstab
-echo -e "/mnt/etc/mysql\t/etc/mysql\tnone\tbind" | sudo tee -a /etc/fstab
-echo -e "/mnt/lib/mysql\t/var/lib/mysql\tnone\tbind" | sudo tee -a /etc/fstab
-echo -e "/mnt/log/mysql\t/var/log/mysql\tnone\tbind" | sudo tee -a /etc/fstab
+sudo mkdir /data
+sudo mount /dev/xvdf /data
+echo -e "/dev/xvdf\t/data\text4\tdefaults,nofail\t0\t2" | sudo tee -a /etc/fstab
 sudo mount -a
+echo -e "/data/etc/mysql\t/etc/mysql\tnone\tbind" | sudo tee -a /etc/fstab
+echo -e "/data/lib/mysql\t/var/lib/mysql\tnone\tbind" | sudo tee -a /etc/fstab
+echo -e "/data/log/mysql\t/var/log/mysql\tnone\tbind" | sudo tee -a /etc/fstab
+sudo mount /var/log/mysql
+sudo mount /etc/mysql
+sudo mount /var/lib/mysql
+sudo chmod 777 -R /data
 sudo service mysql start
 }
 main
