fc0a68fd4d82f488931573ccd796bb0d74b9885d
Ubuntu <ubuntu@ip-172-31-50-94.ec2.internal>
2015-10-10 05:59:14 +0000

    link mysql to ebs

---
+++ TA/mysqlUseEBS.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+main(){
+sudo umount /mnt
+sudo mkdir /mnt/etc /mnt/lib /mnt/log
+sudo mv /etc/mysql     /mnt/etc/
+sudo mv /var/lib/mysql /mnt/lib/
+sudo mv /var/log/mysql /mnt/log/
+sudo mkdir /etc/mysql
+sudo mkdir /var/lib/mysql
+sudo mkdir /var/log/mysql
+echo -e "/mnt/etc/mysql\t/etc/mysql\tnone\tbind" | sudo tee -a /etc/fstab
+sudo mount /etc/mysql
+echo -e "/mnt/lib/mysql\t/var/lib/mysql\tnone\tbind" | sudo tee -a /etc/fstab
+sudo mount /var/lib/mysql
+echo -e "/mnt/log/mysql\t/var/log/mysql\tnone\tbind" | sudo tee -a /etc/fstab
+sudo mount /var/log/mysql
+}
+main
---
+++ TA/startScript.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+main(){
+sudo apt-get -y update
+sudo apt-get -y install openjdk-7-jdk
+sudo apt-get -y install awscli
+sudo apt-get -y install mysql-server
+}
+main
